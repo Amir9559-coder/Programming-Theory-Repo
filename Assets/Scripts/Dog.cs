b09b376c0186453d0cfe3487bc5d349536493b54
Amir9559-coder <rstmam1399@yahoo.com>
2024-09-12 22:53:48 +0330

    first part
    I Completed just dog movement in this part. also i added plane and cow and cow script.

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : PlayerController
+{
+    private float speed = 10;
+    private float rotationSpeed = 150;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer(speed, rotationSpeed);
+        ControllAnimation(playerAnim);
+        Boundary();
+    }
+    protected override void Boundary()
+    {
+        int zBound = 12;
+        int xBound = 12;
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        else if(transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if(transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    protected void MovePlayer(float speed , float rotationSpeed)
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * vertical * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontal * rotationSpeed);
+    }
+    protected abstract void Boundary();
+    protected void ControllAnimation(Animator playerAnim)
+    {
+        if(horizontal == 0 && vertical == 0)
+        {
+            playerAnim.SetFloat("Speed_f", 0.0f);
+        }
+        else
+        {
+            playerAnim.SetFloat("Speed_f", 0.5f);
+        }
+    }
+}
